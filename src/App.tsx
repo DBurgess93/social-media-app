33a8c6b986092eec2b423c1d4790fd7e9cf58392
Daniel Burgess <dan-burgess@hotmail.com>
2023-04-05 09:58:43 +0930

    Install RRD. Create Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Main } from "./pages/main";
+import { Login } from "./pages/login";
+import { Navbar } from "./components/navbar";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
