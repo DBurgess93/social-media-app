8bfe2e6e38e60a2926ef9254e09bbaf81c0a2362
Daniel Burgess <dan-burgess@hotmail.com>
2023-04-11 11:20:33 +0930

    Add function for 'think' reaction to post

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -26,19 +26,32 @@ interface Wow {
   userId: string;
 }
 
+interface Think {
+  thinkId: string;
+  userId: string;
+}
+
 export const Post = (props: Props) => {
   const { post } = props;
   const [user] = useAuthState(auth);
 
   const [likes, setLikes] = useState<Like[] | null>(null);
   const [wows, setWows] = useState<Wow[] | null>(null);
+  const [thinks, setThinks] = useState<Think[] | null>(null);
 
+  // Grabs the full collection from firestore database
   const likesRef = collection(db, "likes");
   const wowsRef = collection(db, "wows");
+  const thinksRef = collection(db, "thinks");
 
+  // Queries the collection and grabs only those objects with the same post IDs
   const likesDoc = query(likesRef, where("postId", "==", post.id));
   const wowsDoc = query(wowsRef, where("postId", "==", post.id));
+  const thinksDoc = query(thinksRef, where("postId", "==", post.id));
 
+  // Await is used to pause functions execution until the getDocs function has fetched the data and stored is it 'data'
+  // The setLikes useState function is then used to map through the fetched 'data', creating a new object for each one (userId & likeId)
+  // The array of objects is now passed through the setLikes function, and therefor 'likes' now represents to new array
   const getLikes = async () => {
     const data = await getDocs(likesDoc);
     setLikes(
@@ -53,6 +66,18 @@ export const Post = (props: Props) => {
     );
   };
 
+  const getThinks = async () => {
+    const data = await getDocs(thinksDoc);
+    setThinks(
+      data.docs.map((doc) => ({ userId: doc.data().userId, thinkId: doc.id }))
+    );
+  };
+
+  // This function uses await to pause the execution until a newDoc is added with the userId and postId
+  // This newDoc (users reaction such as 'likes' this post) is then added to the likes array with the setLikes useState function
+  // Basically if there are already Docs (likes) in the array (prev = previous array content) it will add the newDocs together with the previous Docs
+  // Otherwise it will just add the new Docs
+  // Finally it will remove any other reaction as the user may until react in one way to each post
   const addLike = async () => {
     try {
       const newDoc = await addDoc(likesRef, {
@@ -69,7 +94,8 @@ export const Post = (props: Props) => {
     } catch (err) {
       console.log(err);
     }
-    removeWow()
+    removeWow();
+    removeThink();
   };
   const addWow = async () => {
     try {
@@ -87,9 +113,34 @@ export const Post = (props: Props) => {
     } catch (err) {
       console.log(err);
     }
-    removeLike()
+    removeLike();
+    removeThink();
   };
 
+  const addThink = async () => {
+    try {
+      const newDoc = await addDoc(thinksRef, {
+        userId: user?.uid,
+        postId: post.id,
+      });
+      if (user) {
+        setThinks((prev) =>
+          prev
+            ? [...prev, { userId: user?.uid, thinkId: newDoc.id }]
+            : [{ userId: user?.uid, thinkId: newDoc.id }]
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    removeLike();
+    removeWow();
+  };
+
+  // This first queries the collection of likes for the post in likesRef. Awaits the execution then stores the data in likeToDeleteData.
+  // Stores the id of the specific like in the database as likeId. Matches the likeId to the speific like belonging to the user in the db (this is not likeToDelete)
+  // deleteDoc then take the argument of likeToDelete, then using setLikes it gets the previous array of likes and returns only the likes that do NOT match the likeId
+  // Therefor the like is not longer in the database and has been deleted.
   const removeLike = async () => {
     try {
       const liketoDeleteQuery = query(
@@ -99,8 +150,8 @@ export const Post = (props: Props) => {
       );
       const likeToDeleteData = await getDocs(liketoDeleteQuery);
       const likeId = likeToDeleteData.docs[0].id;
-      const liketoDelete = doc(db, "likes", likeId);
-      await deleteDoc(liketoDelete);
+      const likeToDelete = doc(db, "likes", likeId);
+      await deleteDoc(likeToDelete);
       if (user) {
         setLikes(
           (prev) => prev && prev.filter((like) => like.likeId !== likeId)
@@ -113,15 +164,15 @@ export const Post = (props: Props) => {
 
   const removeWow = async () => {
     try {
-      const wowtoDeleteQuery = query(
+      const wowToDeleteQuery = query(
         wowsRef,
         where("postId", "==", post.id),
         where("userId", "==", user?.uid)
       );
-      const wowToDeleteData = await getDocs(wowtoDeleteQuery);
+      const wowToDeleteData = await getDocs(wowToDeleteQuery);
       const wowId = wowToDeleteData.docs[0].id;
-      const wowtoDelete = doc(db, "wows", wowId);
-      await deleteDoc(wowtoDelete);
+      const wowToDelete = doc(db, "wows", wowId);
+      await deleteDoc(wowToDelete);
       if (user) {
         setWows((prev) => prev && prev.filter((wow) => wow.wowId !== wowId));
       }
@@ -130,14 +181,38 @@ export const Post = (props: Props) => {
     }
   };
 
+  const removeThink = async () => {
+    try {
+      const thinkToDeleteQuery = query(
+        thinksRef,
+        where("postId", "==", post.id),
+        where("userId", "==", user?.uid)
+      );
+      const thinkToDeleteData = await getDocs(thinkToDeleteQuery);
+      const thinkId = thinkToDeleteData.docs[0].id;
+      const thinkToDelete = doc(db, "thinks", thinkId);
+      await deleteDoc(thinkToDelete);
+      if (user) {
+        setThinks(
+          (prev) => prev && prev.filter((think) => think.thinkId !== thinkId)
+        );
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   const hasUserLiked = likes?.find((like) => like.userId === user?.uid);
   const hasUserWowed = wows?.find((wow) => wow.userId === user?.uid);
+  const hasUserThinked = thinks?.find((think) => think.userId === user?.uid);
 
   const userReaction = () => {
     if (hasUserLiked) {
       return <>&#x1F920;</>;
     } else if (hasUserWowed) {
       return <>&#x1F92F;</>;
+    } else if (hasUserThinked) {
+      return <>&#x1F914;</>;
     } else {
       return <>&#x1F636;</>;
     }
@@ -149,6 +224,10 @@ export const Post = (props: Props) => {
 
   useEffect(() => {
     getWows();
+  }, []);
+
+  useEffect(() => {
+    getThinks();
   }, []);
 
   return (
@@ -174,6 +253,15 @@ export const Post = (props: Props) => {
                   className="like-btn"
                 >
                   &#x1F920;
+                </button>
+              </div>
+              <div className="reaction">
+                {thinks && <p>{thinks?.length}</p>}
+                <button
+                  onClick={hasUserThinked ? removeThink : addThink}
+                  className="like-btn"
+                >
+                  &#x1F914;
                 </button>
               </div>
               <div className="reaction">
